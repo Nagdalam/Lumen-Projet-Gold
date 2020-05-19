686063079f8909fda4e535fec96678b7b5f89db0
Arvis <pierre.arvis@edu.devinci.fr>
2020-05-19 10:11:51 +0200

    LevelSelector + textMeshPro

---
+++ Lumen Projet Gold/Assets/Script/Menu/mainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    
+   
+    public void PlayGame (int Id)
+    {
+        SceneManager.LoadScene(Id);
+
+    } 
+    //public void PlaySelector ()
+    //{
+    //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    //}
+  
+  
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+
+}
